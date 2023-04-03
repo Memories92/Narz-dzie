af98a0114366d30802771329986396f858e747b1
Memories92 <dsw50742@student.dsw.edu.pl>
2023-04-03 22:59:21 +0200

    Odswiezenie pomocy, ale jeszcze nie do merdżowania!

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,4 @@
 
 test "${1}" = "--date" || test "${1}" = "-d" && date
 test "${1}" = "--logs" || test "${1}" = "-l" && n=${2:-100} && seq ${n} | while read i; do echo "log${i}.txt | ${0} | $(date)" > log${i}.txt; done
-test "${1}" = "--help" || test "${1}" = "-h" && echo "Dostępne opcje to: $0 --date | --logs <n> (Domyślnie: 100)"
+test "${1}" = "--help" || test "${1}" = "-h" && echo "Dostępne opcje to: $0 --date / -d | -l <n> / --logs <n> (Domyślnie: 100) | --help /-h"
