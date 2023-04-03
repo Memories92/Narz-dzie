8c07ab70b52a1f0ec897081502ae56f922523deb
Memories92 <dsw50742@student.dsw.edu.pl>
2023-04-03 22:48:52 +0200

    Dodaję --help

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,4 @@
 
 test "${1}" = "--date" && date
 test "${1}" = "--logs" && n=${2:-100} && seq ${n} | while read i; do echo "log${i}.txt | ${0} | $(date)" > log${i}.txt; done
+test "${1}" = "--help" && echo "Dostępne opcje to: $0 --date | --logs <n> (Domyślnie: 100)"
