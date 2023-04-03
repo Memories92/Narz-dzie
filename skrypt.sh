b9c0225f08d3f0ba710e46414c985a33c881daca
Memories92 <dsw50742@student.dsw.edu.pl>
2023-04-03 22:44:50 +0200

    robie tak, ze jesli jest cos po --logs, to bedzie to liczba logow, a jak nie to 100

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 test "${1}" = "--date" && date
-test "${1}" = "--logs" && seq 100 | while read i; do echo "log${i}.txt | ${0} | $(date)" > log${i}.txt; done
+test "${1}" = "--logs" && n=${2:-100} && seq ${n} | while read i; do echo "log${i}.txt | ${0} | $(date)" > log${i}.txt; done
