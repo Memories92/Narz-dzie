71797a9c2d6787f526b440a48910a2712d2ce884
Memories92 <dsw50742@student.dsw.edu.pl>
2023-04-03 23:06:23 +0200

    Edycja --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 test "${1}" = "--date" || test "${1}" = "-d" && date
-test "${1}" = "--logs" || test "${1}" = "-l" && n=${2:-100} && seq ${n} | while read i; do echo "log${i}.txt | ${0} | $(date)" > log${i}.txt; done
+test "${1}" = "--logs" || test "${1}" = "-l" && n=${2:-100} && seq ${n} | while read i; do mkdir log${i}; echo "log${i}/log${i}.txt | ${0} | $(date)" > log${i}/log${i}.txt; done
 test "${1}" = "--help" || test "${1}" = "-h" && echo "Dostępne opcje to: $0 --date | --logs <n> (Domyślnie: 100)"
